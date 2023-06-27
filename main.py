d3559391b4e10524c46f6b714d43ec3dbe66b39b
dmbernaal <dmbernaal@gmail.com>
2023-06-27 01:36:44 -0400

    local file process, ugly print and main -> needs betterdesign

--- main.py
+++ main.py
@@ -3,44 +3,31 @@ import os
 from src.audio import url2mp4, split_audio, load_file_as_bytes, wget
 from src.whisper import transcribe_audio_chunks
 from src.vtt import VTT
+from src.prints import Print
 
-if __name__ == "__main__":
-    argparse = argparse.ArgumentParser()
-    argparse.add_argument("--url", type=str, required=True)
-    argparse.add_argument("--output", default="output.txt", type=str, required=True)
-    argparse.add_argument("--verbose", default=True, type=bool, required=False)
-    argparse.add_argument("--save-vtt", default=False, type=bool, required=False)
-    argparse.add_argument("--wget", default=False, type=bool, required=False)
+print = Print()
 
-    args = argparse.parse_args()
-
-    # Download the m3u8 file
-    if args.verbose: print("Downloading file...")
-    if args.wget: m3u8_file_path = wget(args.url)
-    else: m3u8_file_path = url2mp4(args.url)
-
-    # load the m3u8 file as a byte stream
-    if args.verbose: print("Loading file as a byte stream...")
-    m3u8_file = load_file_as_bytes(m3u8_file_path)
-
-    # Split the audio into chunks
-    if args.verbose: print("Splitting the audio into chunks...")
-    audio_chunks = split_audio(m3u8_file, input_audio_format="mp4", output_audio_format="mp3", chunk_duration=60, min_duration_for_chunking=300)
-
-    # Transcribe the audio chunks
-    if args.verbose: print("Transcribing the audio chunks...")
-    transcription = transcribe_audio_chunks(audio_chunks)
-
-    # if VTT is enabled, parse the transcription into a VTT file
+def main(args):
+    if not args.local:
+        print.download()
+        if args.wget: file_path = wget(args.url_or_path)
+        else: file_path = url2mp4(args.url_or_path)
+    else: print.local(); file_path = args.url_or_path
+    print.tobytes(); bytes_file = load_file_as_bytes(file_path)
+    print.split(); audio_chunks = split_audio(bytes_file, input_audio_format="mp4", output_audio_format="mp3", chunk_duration=60, min_duration_for_chunking=300)
+    print.transcribe(); transcription = transcribe_audio_chunks(audio_chunks)
     if args.save_vtt:
         vtt = VTT.from_transcription(transcription)
         transcription = vtt.parse()
+    with open(args.output, "w") as f: print.save(); f.write(transcription)
+    print.remove(); os.remove(file_path)
 
-    # Write the transcription to a file
-    if args.verbose: print("Writing the transcription to a file...")
-    with open(args.output, "w") as f: f.write(transcription)
-
-    # delete video file
-    if args.verbose: print("Deleting the video file...")
-    os.remove("./_temp/output.mp4")
-
+if __name__ == "__main__":
+    argparse = argparse.ArgumentParser()
+    argparse.add_argument("--url-or-path", type=str, required=True)
+    argparse.add_argument("--local", default=False, type=bool, required=False)
+    argparse.add_argument("--output", default="output.txt", type=str, required=True)
+    argparse.add_argument("--save-vtt", default=False, type=bool, required=False)
+    argparse.add_argument("--wget", default=False, type=bool, required=False)
+    args = argparse.parse_args()
+    main(args)
---
+++ src/prints.py
@@ -0,0 +1,8 @@
+class Print:
+    def download(self): print("Starting Downloading...")
+    def local(self): print("No download, using local file...")
+    def tobytes(self): print("Converting to bytes...")
+    def split(self): print("Splitting audio...")
+    def transcribe(self): print("Transcribing audio...")
+    def save(self): print("Saving transcription...")
+    def remove(self): print("Removing file...")
